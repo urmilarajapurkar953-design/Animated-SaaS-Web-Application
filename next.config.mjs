c934a238ba5193a0329428cbbf5d98ef19e6610f
coded by urmi <urmilarajapurkar953@gmail.com>
2026-03-05 00:17:00 +0530

    fix: resolve deployment issues

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,13 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
